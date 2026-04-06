f5f9f714b69200a9f457274019bf06e71355f288
Leema Siby <leemarosesiby.27@gmail.com>
2026-04-06 18:00:34 -0400

    Initialized firebase in main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
